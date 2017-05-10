1dd75eb59bf9e8d2518445416da2017c64b45335
C. Arpe <colin.j.arpe@outlook.com>
2017-05-09 23:46:45 -0400

    Add background image, font and JS functionality

--- Assets/Scripts/scripts.js
+++ Assets/Scripts/scripts.js
@@ -1,3 +1,31 @@
 $(document).ready(function () {
+	$(".section-title").each(function(i)	{
+		$(this).on("click", function()	{
+			if (i > 0) {
+				$(".open").addClass("closed").removeClass("open");
+				$(".section-title-text").eq(i).addClass("open");
+			}
+			moveGrid(i);
+		});
+	});
 
-});
+	function moveGrid (i)	{
+		switch(i)	{
+			case 0:
+				break;
+			case 1:
+				fractionString = "1fr 8fr 1fr 1fr 1fr";
+				break;
+			case 2:
+				fractionString = "1fr 1fr 8fr 1fr 1fr";
+				break;
+			case 3:
+				fractionString = "1fr 1fr 1fr 8fr 1fr";
+				break;
+			case 4:
+				fractionString = "1fr 1fr 1fr 1fr 8fr"
+		}
+		$("#grid-wrapper").css({"grid-template-columns":fractionString});
+	}
+
+});		//	end of document ready function
